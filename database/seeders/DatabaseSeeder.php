1aa6cf1892398a6fcb220e7bc111761964435780
Nomangilldev <nomiahamad@gmail.com>
2025-02-22 14:20:14 +0500

    commit

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,8 +14,6 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
-
         DB::table('users')->insert([
             'user_name' => 'Admin',
             'user_email' => 'admin@gmail.com',
