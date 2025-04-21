105d9b7daf791ebb7e5cb446ae4919f64ff48197
M.Ahsan <mianahsan5570@gmail.com>
2025-04-21 12:34:01 +0500

    layout changes

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -4,16 +4,7 @@
 
 @section('main-content')
 
-    <button data-drawer-target="default-sidebar" data-drawer-toggle="default-sidebar" aria-controls="default-sidebar"
-        type="button"
-        class="inline-flex items-center p-2 mt-2 ms-3 text-sm text-gray-500 rounded-lg sm:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600">
-        <span class="sr-only">Open sidebar</span>
-        <svg class="w-6 h-6" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
-            <path clip-rule="evenodd" fill-rule="evenodd"
-                d="M2 4.75A.75.75 0 012.75 4h14.5a.75.75 0 010 1.5H2.75A.75.75 0 012 4.75zm0 10.5a.75.75 0 01.75-.75h7.5a.75.75 0 010 1.5h-7.5a.75.75 0 01-.75-.75zM2 10a.75.75 0 01.75-.75h14.5a.75.75 0 010 1.5H2.75A.75.75 0 012 10z">
-            </path>
-        </svg>
-    </button>
+
 
     <aside id="default-sidebar"
         class="fixed top-0 left-0 z-40 w-[300px] h-[97vh] transition-transform -translate-x-full sm:translate-x-0"
@@ -579,13 +570,26 @@
         </div>
     </aside>
 
-    <div class="p-4 sm:ml-[300px]">
-        <nav class="bg-white border-gray-200 ">
-            <div class="w-full flex flex-wrap items-center justify-between mx-auto px-4">
-                <h3 class="font-semibold text-2xl">
-                    @yield('nav-title', 'No title')
-                </h3>
-                <div class="flex items-center md:order-2 space-x-3 md:space-x-5 rtl:space-x-reverse">
+    <div class="p-4  sm:ml-[300px]">
+        <nav class="relative bg-white border-gray-200 ">
+
+            <div class="w-full flex items-center justify-between mx-auto px-4">
+                <button data-drawer-target="default-sidebar" data-drawer-toggle="default-sidebar" aria-controls="default-sidebar"
+            type="button"
+            class="inline-flex items-center p-2 mt-2 ms-3 text-sm text-gray-500 rounded-lg sm:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600">
+            <span class="sr-only">Open sidebar</span>
+            <svg class="w-6 h-6" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+                <path clip-rule="evenodd" fill-rule="evenodd"
+                    d="M2 4.75A.75.75 0 012.75 4h14.5a.75.75 0 010 1.5H2.75A.75.75 0 012 4.75zm0 10.5a.75.75 0 01.75-.75h7.5a.75.75 0 010 1.5h-7.5a.75.75 0 01-.75-.75zM2 10a.75.75 0 01.75-.75h14.5a.75.75 0 010 1.5H2.75A.75.75 0 012 10z">
+                </path>
+            </svg>
+        </button>
+                <div class="absolute top-14 left-6 sm:top-2">
+                    <h3 class="font-semibold text-2xl">
+                        @yield('nav-title', 'No title')
+                    </h3>
+                </div>
+                <div class="flex items-center sm:pl-32 md:order-2 space-x-3 md:space-x-5 rtl:space-x-reverse">
                     <p style="text-transform: capitalize">{{ session('user_details.user_role') }}</p>
                     <div>
                         <a href="">
@@ -713,23 +717,14 @@
                         </ul>
                     </div>
 
-                    <button data-collapse-toggle="navbar-user" type="button"
-                        class="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
-                        aria-controls="navbar-user" aria-expanded="false">
-                        <span class="sr-only">Open main menu</span>
-                        <svg class="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none"
-                            viewBox="0 0 17 14">
-                            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                                d="M1 1h15M1 7h15M1 13h15" />
-                        </svg>
-                    </button>
+
                 </div>
                 <div class="items-center justify-between hidden w-full md:flex md:w-auto md:order-1" id="navbar-user">
 
                 </div>
             </div>
         </nav>
-        <div class="mt-6">
+        <div class="mt-14 sm:mt-6">
             @yield('content')
         </div>
     </div>
